45ea3b1ddf84379a650a6cdb82b4a6de4598c5fb
Varun <varun.a.shanbhag@gmail.com>
2018-07-30 00:32:18 +0530

    Completed Course Project 1

--- Course Project 1/app.js
+++ Course Project 1/app.js
@@ -1,15 +1,65 @@
 new Vue({
     el: '#app',
     data: {
-    	playerHealth: 100,
-    	monsterHealth: 100,
+        playerHealth: 100,
+        monsterHealth: 100,
         isGameRunning: false,
     },
     methods: {
-    	startNewGame: function() {
-    		this.playerHealth=100;
-    		this.isGameRunning=true;
-    		this.monsterHealth=100;
-    	},
+        startNewGame: function() {
+            this.playerHealth=100;
+            this.monsterHealth=100;
+            this.isGameRunning=true;
+        },
+        attack: function() {
+            if(this.calculateDamage(3,10)) {
+                return;
+            }
+            this.monsterDamage();
+        },
+        specialAttack: function() {
+            if(this.calculateDamage(6,14)) {
+                return;
+            }
+            this.monsterDamage();
+        },
+        heal: function () {
+            this.monsterDamage();
+            this.playerHealth=Math.min(this.playerHealth+10,100)
+        },
+        giveUp: function() {
+            this.playerHealth=100;
+            this.monsterHealth=100;
+            this.isGameRunning=false;
+        },
+        calculateDamage: function(min,max) {
+            this.monsterHealth -= Math.max(Math.floor(Math.random()*max)+1,min);
+            if(this.checkHealth()) {
+                return true;
+            }
+        },
+        monsterDamage: function() {
+            this.playerHealth -= Math.max(Math.floor(Math.random()*12)+1,5);
+            if(this.checkHealth()) {
+                return true;
+            }
+        },
+        checkHealth: function() {
+            if(this.monsterHealth<=0) {
+                this.checkDecision("Won!");
+                return true;
+            }
+            if(this.playerHealth<=0) {
+                this.checkDecision("Lost");
+            }
+        },
+        checkDecision: function(msg) {
+            if (confirm('You '+msg+' Start New Game?')) {
+                this.startNewGame();
+                return;
+            }
+            this.isGameRunning=false;
+
+        },
     },
 });
