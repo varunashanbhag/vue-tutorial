2f529a045d2ee980eee4100c05d787f9d0e0b23e
Varun Shanbhag <varun.shanbhag@wisdmlabs.com>
2018-11-06 13:00:00 +0530

    refs #000 Finished Components with data Assignment

---
+++ Components Exercise (with Data)/src/components/Server/Servers.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <appSerrver
+                v-for="server in servers"
+                :server="server">
+            </appSerrver>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import Server from './Server.vue';
+    export default {
+        data() {
+            return {
+                servers: [
+                    {id:1, status:"Normal"},
+                    {id:2, status:"Critical"},
+                    {id:3, status:"Unknown"},
+                    {id:4, status:"Normal"},
+                ]
+            }
+        },
+        components: {
+            appSerrver: Server,
+        }
+    }
+</script>
+
+<style>
+
+</style>
